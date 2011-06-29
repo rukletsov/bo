98d5b2c63e4320a11a951492af52714b781325df
Alexander Rukletsov <alexander.rukletsov@ziti.uni-heidelberg.de>
2011-06-29 18:04:20 +0200

    Files "stdafx.h" and "stdafx.cpp" renamed to "pch.h" and "pch.cpp" respectively.

--- libCommon/src/ini_reader.cpp
+++ libCommon/src/ini_reader.cpp
@@ -1,5 +1,5 @@
 
-#include "stdafx.h"
+#include "pch.h"
 
 #include <boost/assert.hpp>
 #include <boost/regex.hpp>
--- libCommon/src/mesh.cpp
+++ libCommon/src/mesh.cpp
@@ -1,5 +1,5 @@
 
-#include "stdafx.h"
+#include "pch.h"
 
 #include <stdexcept>
 #include <boost/assert.hpp>
