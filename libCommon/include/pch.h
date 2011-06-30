d13d1271c4c9aa8802458660a2ec856fd901bfd3
Alexander Rukletsov <alexander.rukletsov@ziti.uni-heidelberg.de>
2011-06-30 15:28:22 +0200

    _WIN32_WINNT macro is set in CMake config.

--- libCommon/include/pch.h
+++ libCommon/include/pch.h
@@ -10,10 +10,17 @@
 // this file will be empty and therefore precompiled headers won't be supported.
 #ifdef _MSC_VER
 
-#ifndef _WIN32_WINNT            // Allow use of features specific to Windows XP or later.
-#   define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other
-#endif                          // versions of Windows.
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+// Allow use of features specific to Windows XP or later. Change this to the
+// appropriate value to target other versions of Windows. Note that this option can
+// be set during configuration process (with CMake). For more information see
+//     http://msdn.microsoft.com/en-us/library/aa383745%28v=vs.85%29.aspx
+#ifndef _WIN32_WINNT
+#   define _WIN32_WINNT 0x0501
+#endif
+
+// Exclude rarely-used stuff from Windows headers. For more information see
+//     http://msdn.microsoft.com/en-us/library/aa383745%28v=vs.85%29.aspx
+#define WIN32_LEAN_AND_MEAN
 
 // Specify headers which will be stored in a precompiled header. USE_PCH variable can
 // be defined by the user during configuration process (with CMake), or explicitly
