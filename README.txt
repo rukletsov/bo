bf2f834a69d5f4cddb65192ef67e821dc4893d4e
Alexander Rukletsov <alexander.rukletsov@ziti.uni-heidelberg.de>
2011-06-30 12:57:16 +0200

    Precompiled headers can be used optionally.

--- README.txt
+++ README.txt
@@ -4,5 +4,5 @@ hg pull https://bitbucket.org/belarus.europe/libcommon
 hg push https://bitbucket.org/belarus.europe/libcommon
 
 CMake command-line options:
--DCMAKE_BUILD_TYPE=Debug -DBuildTests=True
--DCMAKE_BUILD_TYPE=Release -DBuildTests=True
+-DCMAKE_BUILD_TYPE=Debug -DBuildTests=ON -DUsePch=OFF
+-DCMAKE_BUILD_TYPE=Release -DBuildTests=ON -DUsePch=OFF
--- libCommon/include/pch.h
+++ libCommon/include/pch.h
@@ -10,27 +10,26 @@
 // this file will be empty and therefore precompiled headers won't be supported.
 #ifdef _MSC_VER
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
-
+#ifndef _WIN32_WINNT            // Allow use of features specific to Windows XP or later.
+#   define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other
+#endif                          // versions of Windows.
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
-
-
-// Specify headers which will be stored in a precompiled header.
-
-#include <cstddef>
-#include <cmath>
-#include <stdexcept>
-#include <string>
-#include <vector>
-#include <map>
-
-#include <boost/cstdint.hpp>
-#include <boost/assert.hpp>
-#include <boost/array.hpp>
-#include <boost/format.hpp>
+// Specify headers which will be stored in a precompiled header. USE_PCH variable can
+// be defined by the user during configuration process (with CMake), or explicitly
+// before this line. Note that msvc's pch cannot be used in distributed builds.
+#ifdef USE_PCH
+#   include <cstddef>
+#   include <cmath>
+#   include <stdexcept>
+#   include <string>
+#   include <vector>
+#   include <map>
+#   include <boost/cstdint.hpp>
+#   include <boost/assert.hpp>
+#   include <boost/array.hpp>
+#   include <boost/format.hpp>
+#endif // USE_PCH
 
 #endif // _MSC_VER
 
