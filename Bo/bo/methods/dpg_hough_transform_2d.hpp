dbca892f6ee7ad783201f268d842dc4d73de6b20
Dzmitry Hlindzich <dzmitry.hlindzich@ziti.uni-heidelberg.de>
2013-01-12 19:26:12 +0100

    dpg_hough_transform_2d: basic interface added.

--- Bo/bo/methods/dpg_hough_transform_2d.hpp
+++ Bo/bo/methods/dpg_hough_transform_2d.hpp
@@ -34,6 +34,74 @@
 #ifndef DPG_HOUGH_TRANSFORM_HPP_55B0255B_E6C8_4302_9114_D1B684CD3419
 #define DPG_HOUGH_TRANSFORM_HPP_55B0255B_E6C8_4302_9114_D1B684CD3419
 
+#include <utility>
+#include <vector>
 
+#include "bo/vector.hpp"
+
+namespace bo {
+namespace methods {
+
+namespace recognition {
+
+template <typename RealType>
+class DualPointGHT: public boost::noncopyable
+{
+public:
+    typedef DualPointGHT<RealType> this_type;
+    typedef Vector<RealType, 2> Point2D;
+    typedef std::vector<Point2D> Points2D;
+    typedef std::pair<Point2D, Point2D> Reference;
+    typedef std::pair<Point2D, Point2D> RecognitionArea;
+    typedef std::pair<Reference, unsigned int> ReferenceVote;
+    typedef std::vector<ReferenceVote> ReferenceVotes;
+    typedef std::pair<Point2D, Point2D> Feature;
+    typedef std::vector<Feature> Features; 
+    typedef std::pair<RealType, RealType> ATableElement;
+    typedef std::vector<std::list<ATableElement> > ATable;
+     
+
+    DualPointGHT(const Features &model_features, const Reference &model_reference):
+                model_reference_(model_reference)
+    {
+        encode(model_features, model_reference);
+    }
+
+    // Detects the references that define probable poses of the model within the 
+    // given features.
+    ReferenceVotes fast_detect(const Features &object_features, RealType probability, 
+                               unsigned int T, unsigned int M, RecognitionArea bounding_box) 
+    {
+        ReferenceVotes ref_votes;
+
+        return ref_votes;
+    }
+
+    // Reconstructs the points of the model that has the pose defined by the given reference.
+    Points2D reconstruct(const Reference &reference_points)
+    {
+        Points2D points;
+
+        return points;
+    }
+
+private:
+
+    Reference model_reference_;
+    ATable atable_;
+
+    // Fills in the alpha-table using the given model features
+    // and two reference points.
+    void encode(const Features &model_features, const Reference &model_reference)
+    {
+        
+    }
+
+};
+
+} // namespace recognition.
+
+} // namespace methods.
+} // namespace bo.
 
 #endif //DPG_HOUGH_TRANSFORM_HPP_55B0255B_E6C8_4302_9114_D1B684CD3419
