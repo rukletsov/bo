6e6f258f58989e65b250cccf5088f49b40ee2b24
dzmitry <dzmitry@ThinkPad-T430u>
2013-01-13 23:18:45 +0100

    dpg_hough_transform_2d: Space functionality started.

--- Bo/bo/methods/dpg_hough_transform_2d.hpp
+++ Bo/bo/methods/dpg_hough_transform_2d.hpp
@@ -39,6 +39,7 @@
 #include <vector>
 #include <list>
 #include <cmath>
+#include <algorithm>
 
 #include <boost/noncopyable.hpp>
 #include <boost/math/constants/constants.hpp>
@@ -52,6 +53,145 @@ namespace bo {
 namespace methods {
 namespace recognition {
 
+namespace detail{
+
+template <typename RealType>
+class Space
+{
+public:
+
+    typedef Vector<RealType, 4> Point4D;
+    typedef std::pair<Point4D, Point4D> Box4D;
+    typedef std::pair<Point4D, Point4D> Line4D;
+    typedef Vector<RealType, 2> SegmentCoordinates;
+    typedef std::vector<Space> Spaces;
+    // A segment in 4D is modelled by a 4D line and the coordinates of two points on this line
+    // relatively to the line's direction vector.
+    typedef std::pair<Line4D, SegmentCoordinates> Segment4D;
+
+    Space(const Box4D &box = Box4D(Point4D(0,0,0,0), Point4D(0,0,0,0)), std::size_t resolution_level = 0):
+        box_(box), resolution_level_(resolution_level), votes_(0)
+    {
+    }
+
+    // Subdivides the space.
+    void subdivide(std::size_t divisions_per_dimension)
+    {
+        subspaces_.resize(divisions_per_dimension * divisions_per_dimension *
+                          divisions_per_dimension * divisions_per_dimension);
+
+        Point4D sizes4d = box_.second - box_.first;
+        Point4D steps4d = sizes4d / divisions_per_dimension;
+
+        for (std::size_t d0 = 0; d0 < divisions_per_dimension; ++d0)
+            for (std::size_t d1 = 0; d1 < divisions_per_dimension; ++d1)
+                for (std::size_t d2 = 0; d2 < divisions_per_dimension; ++d2)
+                    for (std::size_t d3 = 0; d3 < divisions_per_dimension; ++d3)
+                    {
+                        // Create a subspace.
+                        Point4D translation(d0 * steps4d[0], d1 * steps4d[1], d2 * steps4d[2], d3 * steps4d[3]);
+                        Box4D b(box_.first + translation, box_.first + translation + steps4d);
+                        Space s(b, resolution_level_ + 1);
+
+                        subspaces_.push_back(s);
+                    }
+
+    }
+
+    const Spaces& get_subspaces()
+    {
+        return subspaces_;
+    }
+
+    const Box4D& get_bounding_box()
+    {
+        return box_;
+    }
+
+    RealType get_votes()
+    {
+        return votes_;
+    }
+
+    Point4D get_mass_center()
+    {
+        return (box_.first + box_.second) / 2;
+    }
+
+    void intersect(Line4D line)
+    {
+        // Create the "infinite" segment coordinates that models the whole line.
+        SegmentCoordinates coord(std::numeric_limits<RealType>::min(), std::numeric_limits<RealType>::max());
+
+        // Create the "infinite" segment.
+        Segment4D seg(line, coord);
+
+        // Cut the segment in each of four dimensions.
+        for (std::size_t d = 0; d < 4; ++d)
+            cut_segment(seg, d, box_.first[d], box_.second[d]);
+
+        // The number of votes that the space receives in the result of the intersection
+        // equals to the lenght of the resulting segment.
+        // Compute the segment.
+        Point4D s = seg.first.second * (seg.second[1] - seg.second[0]);
+        // Increase the votes.
+        votes_ += s.euclidean_norm();
+    }
+
+private:
+
+    Spaces subspaces_;
+    Box4D box_;
+    std::size_t resolution_level_;
+    RealType votes_;
+
+
+    // Cuts the segment in the given dimension according to two given levels.
+    void cut_segment(Segment4D &seg, std::size_t dimension, RealType level1, RealType level2)
+    {
+        // Sort the levels: level1 <= level2.
+        if (level1 > level2)
+            std::swap<RealType>(level1, level2);
+
+        // The origin point and the direction.
+        Point4D p = seg.first.first;
+        Point4D v = seg.first.second;
+
+        // If the segment is parallel to the levels of this dimension.
+        if (v[dimension] == 0)
+        {
+             // If the origin is outside the levels, return zero segment.
+            if (p[dimension] > level2 || p[dimension] < level1)
+                seg.second[0] = seg.second[1] = 0;
+
+            // Otherwise return the same segment.
+            return;
+        }
+
+        // Define the coordinates of the line instersection with the dimension levels.
+        RealType t1 = (level1 - p[dimension]) / v[dimension];
+        RealType t2 = (level2 - p[dimension]) / v[dimension];
+
+        // Sort the coordinates: t1 <= t2.
+        if (t1 > t2)
+            std::swap<RealType>(t1, t2);
+
+        // Cut the segment.
+        for (int i = 0; i < 2; ++i)
+        {
+            if (seg.second[i] < t1)
+                seg.second[i] = t1;
+
+            if (seg.second[i] > t2)
+                seg.second[i] = t2;
+        }
+
+    }
+};
+
+
+} // namespace detail
+
 template <typename RealType>
 class DualPointGHT: public boost::noncopyable
 {
@@ -61,7 +201,7 @@ public:
     typedef std::vector<Point2D> Points2D;
     typedef std::pair<Point2D, Point2D> Reference;
     typedef std::pair<Point2D, Point2D> RecognitionArea;
-    typedef std::pair<Reference, unsigned int> ReferenceVote;
+    typedef std::pair<Reference, std::size_t> ReferenceVote;
     typedef std::vector<ReferenceVote> ReferenceVotes;
     typedef std::pair<Point2D, Point2D> Feature;
     typedef std::vector<Feature> Features; 
@@ -82,7 +222,35 @@ public:
     ReferenceVotes fast_detect(const Features &object_features, RealType probability, 
                                unsigned int T, unsigned int M, RecognitionArea bounding_box) 
     {
+        using namespace detail;
+
         ReferenceVotes ref_votes;
+
+        // Create the root space object.
+        typename Space<RealType>::Point4D p1(bounding_box.first[0], bounding_box.first[1],
+                                  bounding_box.first[0], bounding_box.first[1]);
+        typename Space<RealType>::Point4D p2(bounding_box.second[0], bounding_box.second[1],
+                                  bounding_box.second[0], bounding_box.second[1]);
+        space_ = Space<RealType>(Space<RealType>::Box4D(p1, p2));
+
+        // Hierarchical search for the vote peak in the space.
+        process_space(space_, T, M);
+
+        /*
+        // Get all subspaces from the last resolution level;
+        typename Space<RealType>::Spaces leafs = get_resolution_level(M);
+
+        // Extract the references.
+        for (typename Space<RealType>::const_iterator it = leafs.begin(); it != leafs.end(); ++it)
+        {
+            // Attention: the space is approximated by its mass center!
+            typename Space<RealType>::Point4D c = it->get_mass_center();
+
+            Reference ref(Point2D(c[0], c[1]), Point2D(c[2], c[3]));
+            ReferenceVote rv(ref, it->get_votes());
+            ref_votes.pop_back(rv);
+        }
+        */
 
         return ref_votes;
     }
@@ -99,7 +267,7 @@ public:
             for (typename DualPointGHT<RealType>::ATableRow::const_iterator it = ait->begin();
                  it != ait->end(); ++it)
             {
-                points.push_back(find_intersection2(reference_points, *it));
+                points.push_back(find_intersection(reference_points, *it));
             }
 
         return points;
@@ -110,7 +278,7 @@ private:
     Reference model_reference_;
     RealType tangent_accuracy_;
     ATable atable_;
-
+    detail::Space<RealType> space_;
     RealType pi_;
 
     // Row index in the alpha-table for the given tangent angle.
@@ -156,7 +324,7 @@ private:
 
     // Computes the intersection of two lines, defined by the reference points and
     // the given element of the alpha-table.
-    Point2D find_intersection2(const Reference &reference_points, const ATableElement &e)
+    Point2D find_intersection(const Reference &reference_points, const ATableElement &e)
     {
         Point2D p;
 
@@ -167,7 +335,7 @@ private:
         // Reference vector.
         Point2D ab = p2 - p1;
 
-        // If the intersection is located on the reference line compute the intersection
+        // If the intersection is located on the reference line, compute the intersection
         // directly.
         if (e.first == 0)
         {
